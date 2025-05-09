e326a5c193cab704790c96e46f73a080538c2050
mhijazi16 <gultimate0@gmail.com>
2025-05-09 23:03:44 +0300

    map.go

--- map.go
+++ map.go
@@ -1,26 +1,26 @@
-package main
-
-import (
-	"fmt"
-)
-
-func main() {
-
-	var humans = make(map[string]uint)
-	humans = map[string]uint{"jake": 12, "Mohammed": 60, "john": 34}
-
-	var age, in_map = humans["jake"]
-	if in_map {
-		fmt.Printf("jake age: %v\n", age)
-		delete(humans, "jake")
-	}
-
-	for name, age := range humans {
-		fmt.Printf("Name: %v, Age: %v\n", name, age)
-	}
-
-	var arr = []int{1, 3, 4}
-	for index, val := range arr {
-		fmt.Printf("arr[%v]=%v\n", index, val)
-	}
-}
+// package main
+//
+// import (
+// 	"fmt"
+// )
+//
+// func main() {
+//
+// 	var humans = make(map[string]uint)
+// 	humans = map[string]uint{"jake": 12, "Mohammed": 60, "john": 34}
+//
+// 	var age, in_map = humans["jake"]
+// 	if in_map {
+// 		fmt.Printf("jake age: %v\n", age)
+// 		delete(humans, "jake")
+// 	}
+//
+// 	for name, age := range humans {
+// 		fmt.Printf("Name: %v, Age: %v\n", name, age)
+// 	}
+//
+// 	var arr = []int{1, 3, 4}
+// 	for index, val := range arr {
+// 		fmt.Printf("arr[%v]=%v\n", index, val)
+// 	}
+// }

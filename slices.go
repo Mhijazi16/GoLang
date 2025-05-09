ec80ead9be9924611b2aef2068fd591fbebd5551
mhijazi16 <gultimate0@gmail.com>
2025-05-09 22:38:05 +0300

    slices.go

---
+++ slices.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var slice = []int{1, 2, 3, 4}
+	fmt.Printf("length: %v, capacity: %v\n", len(slice), cap(slice))
+
+	slice = append(slice, 3)
+	fmt.Printf("length: %v, capacity: %v\n", len(slice), cap(slice))
+
+	var slice2 = []int{0, 0, 0, 0, 0}
+	slice = append(slice, slice2...)
+	fmt.Printf("length: %v, capacity: %v\n", len(slice), cap(slice))
+
+	var slice3 []int = make([]int, 2, 9)
+	fmt.Printf("length: %v, capacity: %v\n", len(slice3), cap(slice3))
+}

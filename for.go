b1158ae840f9390e2b776114e6ce3a3b435815aa
mhijazi16 <gultimate0@gmail.com>
2025-05-09 21:01:07 +0300

    for.go

---
+++ for.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+
+	for i := 1; i <= 3; i++ {
+		fmt.Println("hello")
+	}
+
+	for i := range 3 {
+		fmt.Println("jam", i)
+	}
+
+	for {
+		fmt.Println("looping")
+		break
+	}
+
+	for i := range 10 {
+		if i%2 == 0 {
+			continue
+		}
+		fmt.Println("odd number")
+	}
+}

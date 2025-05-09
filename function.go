0505e4465e3dcf43feea2e3533215d32d2181a61
mhijazi16 <gultimate0@gmail.com>
2025-05-09 22:28:14 +0300

    arrays2.go

--- function.go
+++ function.go
@@ -1,32 +1,32 @@
-package main
-
-import (
-	"errors"
-	"fmt"
-)
-
-func intDivision(numerator int, denomirator int) (int, int, error) {
-	if denomirator == 0 {
-		var err = errors.New("division by zero not allowed")
-		return 0, 0, err
-	}
-
-	return numerator / denomirator, numerator % denomirator, nil
-}
-
-func main() {
-
-	var arr = [2][2]int{{5, 2}, {0, 0}}
-
-	for i := range 2 {
-		var first, second, err = intDivision(arr[i][0], arr[i][1])
-
-		if err != nil {
-			fmt.Printf("failure! resulsts %v %v\n", first, second)
-			fmt.Println(err.Error())
-		} else {
-			fmt.Printf("success resulsts %v %v", first, second)
-		}
-	}
-
-}
+// package main
+//
+// import (
+// 	"errors"
+// 	"fmt"
+// )
+//
+// func intDivision(numerator int, denomirator int) (int, int, error) {
+// 	if denomirator == 0 {
+// 		var err = errors.New("division by zero not allowed")
+// 		return 0, 0, err
+// 	}
+//
+// 	return numerator / denomirator, numerator % denomirator, nil
+// }
+//
+// func main() {
+//
+// 	var arr = [2][2]int{{5, 2}, {0, 0}}
+//
+// 	for i := range 2 {
+// 		var first, second, err = intDivision(arr[i][0], arr[i][1])
+//
+// 		if err != nil {
+// 			fmt.Printf("failure! resulsts %v %v\n", first, second)
+// 			fmt.Println(err.Error())
+// 		} else {
+// 			fmt.Printf("success resulsts %v %v", first, second)
+// 		}
+// 	}
+//
+// }

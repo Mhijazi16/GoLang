1deb49acbd23460db6981b7959a7ebd56575d68b
mhijazi16 <gultimate0@gmail.com>
2025-05-10 23:21:23 +0300

    ddd

--- time.go
+++ time.go
@@ -1,26 +1,26 @@
-package main
-
-import (
-	"fmt"
-	"time"
-)
-
-func timeLoop(slice []int, n int) {
-	var start = time.Now()
-
-	for len(slice) < n {
-		slice = append(slice, 0)
-	}
-	fmt.Printf("time taken: %v\n", time.Since(start))
-}
-
-func main() {
-
-	n := 10000000
-	var one = make([]int, 1, n)
-	var two = []int{}
-
-	timeLoop(one, n)
-	timeLoop(two, n)
-
-}
+// package main
+//
+// import (
+// 	"fmt"
+// 	"time"
+// )
+//
+// func timeLoop(slice []int, n int) {
+// 	var start = time.Now()
+//
+// 	for len(slice) < n {
+// 		slice = append(slice, 0)
+// 	}
+// 	fmt.Printf("time taken: %v\n", time.Since(start))
+// }
+//
+// func main() {
+//
+// 	n := 10000000
+// 	var one = make([]int, 1, n)
+// 	var two = []int{}
+//
+// 	timeLoop(one, n)
+// 	timeLoop(two, n)
+//
+// }

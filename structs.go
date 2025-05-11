3270373cb281f901042be6f9743443c47e6bb7cf
mhijazi16 <gultimate0@gmail.com>
2025-05-11 11:07:54 +0300

    struct.go

---
+++ structs.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+)
+
+type engine interface {
+	milesLeft(float32) float32
+}
+
+func canMakeIt(eg engine, distance float32) float32 {
+	return eg.milesLeft(distance) * 10
+}
+
+type gasEngine struct {
+	speed   float32
+	gallons int32
+}
+
+func (self gasEngine) milesLeft(distance float32) float32 {
+	return self.speed / distance
+}
+
+type electricalEngine struct {
+	speed       float32
+	accelration float32
+}
+
+func (self electricalEngine) milesLeft(distance float32) float32 {
+	return (self.speed * self.accelration) / distance
+}
+
+func main() {
+
+	var one = gasEngine{speed: 20}
+	var two = electricalEngine{speed: 20, accelration: 2}
+
+	fmt.Println(canMakeIt(one, 100))
+	fmt.Println(canMakeIt(two, 100))
+}
